b3e4fd37168ff3a349cf284a29b418b0cd68dfa3
Diztro <diztro.91@googlemail.com>
2020-05-12 12:08:56 +0000

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-  
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you want to use :null_session instead.
+  protect_from_forgery with: :exception
+  include SessionsHelper
 end
